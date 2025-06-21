d5bbada20b5ba505155e47e557de6c25f5b70b0d
donggyun-han <hdg5639@daum.net>
2025-06-21 21:55:21 +0900

    스프링 버전업
    Signed-off-by: donggyun-han <hdg5639@daum.net>

--- AccountServer/src/main/java/com/f5/accountserver/Service/Communication/CommunicationService.java
+++ AccountServer/src/main/java/com/f5/accountserver/Service/Communication/CommunicationService.java
@@ -3,6 +3,6 @@ package com.f5.accountserver.Service.Communication;
 import java.net.URISyntaxException;
 
 public interface CommunicationService {
-    String getEmail(Long id) throws URISyntaxException;
+    String getEmail(String name) throws URISyntaxException;
     Long searchInfo(String username) throws URISyntaxException;
 }
